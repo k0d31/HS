17e4ac6c92e4ab4d09c6b90b7384f01afe1a42eb
skyxgods <31897040+k0d31@users.noreply.github.com>
2025-07-19 17:43:32 +0200

    main.py

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
 import disnake
+from flask import Flask
 from config import config
 from dotenv import load_dotenv
 from disnake.ext import commands
 
 load_dotenv()
+app = Flask(__name__)
 
 intents = disnake.Intents.all()
 bot = commands.Bot(command_prefix = "!", intents = intents)
+
+@app.route("/")
+def home():
+    return "Bot is running!"
+
+# Запускаем Flask сервер в потоке
+def run_flask():
+    app.run(host="0.0.0.0", port=3000)
 
 @bot.event
 async def on_ready():
@@ -20,7 +30,11 @@ async def on_ready():
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
+
         bot.load_extension(f'cogs.{filename[:-3]}')
+
+# Запускаем бота
+bot.loop.create_task(run_flask())
 
 try:
     bot.run(config['token'])
